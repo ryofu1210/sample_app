2fedcf725d71823e59f6a4e963d5afed56e9c997
ryofu1210 <fujii@olivastyle.co.jp>
2019-07-02 15:34:53 +0900

    database 設定変更

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Post, type: :model do
   it "1 + 1 は 2" do
     expect(1 + 1).to eq 2
   end
+
+  it "データベース接続テスト" do
+    post = FactoryBot.create(:post)
+    expect(post).to be_valid
+    expect(post.title).to eq "title"
+  end
 end
